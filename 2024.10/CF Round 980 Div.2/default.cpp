47dd65ac59ee98ad95f2d434d0351ba641ef2390
Wayne233333 <2900199390@qq.com>
2024-10-24 16:29:33 +0800

    202410241659

---
+++ 2024.10/CF Round 980 Div.2/default.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int t, n;
+long long a[400001];
+int b[400001]
+
+int main() {
+    scanf("%d", t);
+    for(int i = 1; i <= t; i++){
+        scanf("%d", n);
+        
+    }
+
+    return 0;
+}
