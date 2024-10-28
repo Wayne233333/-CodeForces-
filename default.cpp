e7cc75fe38e950fb325d9fce6df2375b6aa44afd
Wayne233333 <2900199390@qq.com>
2024-10-28 22:51:19 +0800

    202410282251

--- default.cpp
+++ default.cpp
@@ -14,22 +14,24 @@ int main() {
     int damage;
     double pre_times;
     scanf("%d", &t);
-    for(int i = 1; i >= t; i++){
+    for(int i = 1; i <= t; i++){
         scanf("%d%d%d%d", &x, &y, &z, &k);
         times = 0;
         coins = 0;
-        min_coins = 2e9;[
+        min_coins = 2147483647;
         pre_times = add_damage(x, y, z);
-        while(double((times + 1 )*k) < pre_times){
+        while(double((times + 1 )*k) <= pre_times){
             times++;
             coins += x*k + y;
             z -= k*times;
             pre_times = add_damage(x, y, z);
         }
-        for(int j = times*k + 1; j <= (times + 1) * k; j++){
-            damage = (j-times*k)*x + int(z/j)*y;
-            if(z%j) damage += y;
-            min_coins = min(min_coins, damage);
+        for(int j = times*k; j <= (times + 1) * k; j++){
+            if(j){
+                damage = (j-times*k)*x + int(z/j)*y;
+                if(z%j) damage += y;
+                min_coins = min(min_coins, damage);
+            }
         }
         coins += min_coins;
         printf("%d\n", coins);
